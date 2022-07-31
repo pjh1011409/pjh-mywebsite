5f7a4a7568be61cc89ff051be7fc0cef2114ae46
ParkJeongHo <pjh31208@naver.com>
2022-07-31 21:16:41 +0900

    서버 로컬 호스트 설정중

--- Server/myapp/myapp/settings/base.py
+++ Server/myapp/myapp/settings/base.py
@@ -16,7 +16,7 @@ from django.conf.locale.ko import formats as ko_formats
 ko_formats.DATATIME_FORMAT='Y-m-d G:i:s'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
