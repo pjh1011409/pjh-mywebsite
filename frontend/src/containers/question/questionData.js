2e2099c1a2abb3dfe0ef104c7de3c6d6e35472f7
ParkJeongHo <pjh31208@naver.com>
2022-08-11 00:06:56 +0900

    글 작성

--- frontend/src/containers/question/questionData.js
+++ frontend/src/containers/question/questionData.js
@@ -1,7 +1,7 @@
 const QuestionData = [
     {
         id: 0,
-        question: '내가 개발자가 되고 싶은 이유?',
+        question: '내가 개발자가 되고 싶은 ',
         answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.',
     },
     {
