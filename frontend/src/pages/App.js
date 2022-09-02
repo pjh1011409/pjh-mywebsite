d3c5e05fb9e1fb5f11a0c22e5f862dd3b97f7ffd
ParkJeongHo <pjh31208@naver.com>
2022-09-02 17:11:49 +0900

    ESLint & Prettier

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,105 +1,107 @@
 // ----------------react & hooks-------------------------------------
-import { Routes, Route } from 'react-router-dom'
-
+import { Routes, Route } from 'react-router-dom';
+import React from 'react';
 // ----------------style & css---------------------------------------
-import './App.css'
+import './App.css';
 
 // ----------------components & data---------------------------------
-import MainPage from './mainPage/mainPage'
-import LoginPage from './loginPage/loginPage'
-import { StudyDetail, StudyWrite, StudyUpdate, StudyMain } from './studyPage'
-import { Header, LoginRecordHeader } from 'components/common'
-import Test from 'test'
-//------------------authentication(utils & context)---------------------------
-import PrivateRoute from 'utils/PrivateRoute'
-import { AuthProvider } from 'context/AuthContext'
+import { Header, LoginRecordHeader } from 'components/common';
+import Test from 'test';
+// ------------------authentication(utils & context)---------------------------
+import PrivateRoute from 'utils/PrivateRoute';
+import { AuthProvider } from 'context/AuthContext';
+import { StudyDetail, StudyWrite, StudyUpdate, StudyMain } from './studyPage';
+import LoginPage from './loginPage/loginPage';
+import MainPage from './mainPage/mainPage';
 
 function App() {
-    return (
-        <div style={{ margin: '0', width: '100vm', overflowX: 'hidden' }}>
-            <Routes>
-                <Route
-                    exact
-                    path="/studyMain/*"
-                    element={
-                        <AuthProvider>
-                            <StudyMain>
-                                <Header></Header>
-                            </StudyMain>
-                        </AuthProvider>
-                    }
-                />
+  return (
+    <div style={{ margin: '0', width: '100vm', overflowX: 'hidden' }}>
+      <Routes>
+        <Route
+          exact
+          path="/studyMain/*"
+          element={
+            <AuthProvider>
+              <PrivateRoute>
+                <StudyMain>
+                  <Header />
+                </StudyMain>
+              </PrivateRoute>
+            </AuthProvider>
+          }
+        />
 
-                <Route
-                    exact
-                    path="/studyDetail/:id"
-                    element={
-                        <AuthProvider>
-                            <PrivateRoute>
-                                <StudyDetail></StudyDetail>
-                            </PrivateRoute>
-                        </AuthProvider>
-                    }
-                />
+        <Route
+          exact
+          path="/studyDetail/:id"
+          element={
+            <AuthProvider>
+              <PrivateRoute>
+                <StudyDetail />
+              </PrivateRoute>
+            </AuthProvider>
+          }
+        />
 
-                <Route
-                    exact
-                    path="/"
-                    element={
-                        <AuthProvider>
-                            <MainPage>
-                                <Header></Header>
-                            </MainPage>
-                        </AuthProvider>
-                    }
-                ></Route>
-                <Route
-                    exact
-                    path="/login"
-                    element={
-                        <AuthProvider>
-                            <LoginPage>
-                                <LoginRecordHeader></LoginRecordHeader>
-                            </LoginPage>
-                        </AuthProvider>
-                    }
-                />
-                <Route
-                    exact
-                    path="/studyWrite"
-                    element={
-                        <AuthProvider>
-                            <PrivateRoute>
-                                <StudyWrite />
-                            </PrivateRoute>
-                        </AuthProvider>
-                    }
-                />
-                <Route
-                    exact
-                    path="/studyUpdate/:id"
-                    element={
-                        <AuthProvider>
-                            <PrivateRoute>
-                                <StudyUpdate />
-                            </PrivateRoute>
-                        </AuthProvider>
-                    }
-                />
-                <Route
-                    exact
-                    path="/test"
-                    element={
-                        <AuthProvider>
-                            <PrivateRoute>
-                                <Test />
-                            </PrivateRoute>
-                        </AuthProvider>
-                    }
-                ></Route>
-            </Routes>
-        </div>
-    )
+        <Route
+          exact
+          path="/"
+          element={
+            <AuthProvider>
+              <MainPage>
+                <Header />
+              </MainPage>
+            </AuthProvider>
+          }
+        />
+        <Route
+          exact
+          path="/login"
+          element={
+            <AuthProvider>
+              <LoginPage>
+                <LoginRecordHeader />
+              </LoginPage>
+            </AuthProvider>
+          }
+        />
+        <Route
+          exact
+          path="/studyWrite"
+          element={
+            <AuthProvider>
+              <PrivateRoute>
+                <StudyWrite />
+              </PrivateRoute>
+            </AuthProvider>
+          }
+        />
+        <Route
+          exact
+          path="/studyUpdate/:id"
+          element={
+            <AuthProvider>
+              <PrivateRoute>
+                <StudyUpdate />
+              </PrivateRoute>
+            </AuthProvider>
+          }
+        />
+        <Route
+          exact
+          path="/test"
+          element={
+            <AuthProvider>
+              <PrivateRoute>
+                <Test />
+              </PrivateRoute>
+            </AuthProvider>
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/test.js
+++ frontend/src/test.js
@@ -1,39 +1,36 @@
-import React, {useState, useEffect, useContext} from 'react'
-import AuthContext from '../src/context/AuthContext'
-import useAxios from '../src/utils/useAxios'
+import React, { useState, useEffect } from 'react';
+// import AuthContext from 'src/context/AuthContext';
+import useAxios from '../src/utils/useAxios';
 
 const Test = () => {
-                let [notes, setNotes] = useState([])
-                let {authTokens, logoutUser} = useContext(AuthContext)
-            
-                let api = useAxios()
-            
-                useEffect(()=> {
-                    getNotes()
-                }, [])
-            
-            
-                let getNotes = async() =>{
-                    let response = await api.get('/api/notes/')
-            
-                    if(response.status === 200){
-                        setNotes(response.data)
-                    }
-                    
-                }
-            
-                return (
-                    <div>
-                        <p>You are logged to the home page!</p>
-            
-            
-                        <ul>
-                            {notes.map(note => (
-                                <li key={note.id} >{note.body}</li>
-                            ))}
-                        </ul>
-                    </div>
-                )
-}
+  const [notes, setNotes] = useState([]);
+  //   const { authTokens, logoutUser } = useContext(AuthContext);
 
-export default Test
+  const api = useAxios();
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  let getNotes = async () => {
+    let response = await api.get('/api/notes/');
+
+    if (response.status === 200) {
+      setNotes(response.data);
+    }
+  };
+
+  return (
+    <div>
+      <p>You are logged to the home page!</p>
+
+      <ul>
+        {notes.map(note => (
+          <li key={note.id}>{note.body}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Test;
