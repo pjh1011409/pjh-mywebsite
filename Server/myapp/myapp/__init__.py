43e6b5cc4ced91005d53bed55b75daa9bd74e582
ParkJeongHo <pjh31208@naver.com>
2023-01-03 14:35:03 +0900

    remove : Delete files

--- Server/myapp/myapp/__init__.py
+++
@@ -1,3 +0,0 @@
-# https://github.com/django/channels/issues/969#issuecomment-596746621
-## /mysite/__init__.py
-
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,7 +1,6 @@
-export { default as Header } from './Header/mainHeader/header';
+export { default as Header } from './Header/header';
 export { default as Footer } from './Footer/footer';
 
 export { default as Modal } from './Modal/modal';
 export { default as Pagination } from './Pagination/pagination';
 export { ProjectSlide, StudyingSlide } from './Slide/slide';
-export { default as TextEditor } from './TextEditor/textEditor';
