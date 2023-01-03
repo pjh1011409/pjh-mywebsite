84562cfed4c6ac76308027a571c529d5b0991bfa
ParkJeongHo <pjh31208@naver.com>
2023-01-03 13:58:01 +0900

    remove : Delete ignore files

--- Server/myapp/api/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import *
-  
-class NoteForm(forms.ModelForm):
-  
-    class Meta:
-        model = Note
-        fields = ['category', 'title', 'subTitle','body','image']
--- Server/myapp/api/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello World"] # python3
